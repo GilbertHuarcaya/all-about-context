e62a4c1e6cb26b04cdbc3b5a474ddce6b78731ae
Gilbert Huarcaya <gilberthuarcaya@gmail.com>
2022-01-05 14:28:03 -0500

    index with context

---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { AppProvider } from './context/store';
+import App from './App';
+import './index.scss';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <AppProvider>
+      <App />
+    </AppProvider>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
